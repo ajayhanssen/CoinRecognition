d533e5ebec1a2b6e12f820fc53ccd274af5f1330
ajayhanssen <andi.thoeni@gmail.com>
2025-10-31 11:30:33 +0100

    da du hiniger

---
+++ die_bessere_version_du_gschoderter.m
@@ -0,0 +1,39 @@
+clear all
+close all
+dina4 = [210,297];
+
+% source functions from lecturer
+addpath(genpath('Vorlagen/MatlabFns/Projective'));
+
+img = imread("images/coins1.jpg");
+persp = img;
+
+% origin points
+x1 = [2000 3696 2216 128;
+      1328 2080 4824 3304;
+        1   1   1   1;];
+
+% target points
+x2 = [ 20 3840 3840  20;
+       20 20   5120  5120;
+        1   1   1   1;];
+% create H-matrix
+Hsc = homography2d(x1, x2);
+
+% perspective transformed image
+persp = imTrans(img,Hsc);
+
+BW = rgb2gray(persp);
+BW = imgaussfilt(BW, 8);
+BW = edge(BW,'canny');
+figure(1)
+imshow(BW)
+
+
+
+% figure(2)
+% tiledlayout(1,2)
+% nexttile
+% imshow(img)
+% nexttile
+% imshow(persp)
